dd539593d7cb9bafcb4552562b052cfab698bf75
Philip Demand <philipdemand@yahoo.com>
2023-07-27 11:02:32 -0400

    commit tree c4c8b0a1bf545f46b6610f8fa1d8d87826be1bd8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'Brown', 'Rough', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Gentle', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'White', 'Friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'M', 'Brown', 'Aggressive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Black', 'Angry', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'White', 'Altruistic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Brown', 'Generous', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, 'M', 'Black', 'Charitable', TRUE);
